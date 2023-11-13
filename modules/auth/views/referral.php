614700186a2205e0373126bc4049741908fc1d88
Deni Darmayana <74420243+denidarmayana@users.noreply.github.com>
2023-11-13 13:23:19 +0700

    main

--- modules/auth/views/referral.php
+++ modules/auth/views/referral.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Delta Crypto Trading Bot</title>
+    <title>Crypto Trading Bot</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" type="text/css" rel="stylesheet">
@@ -13,7 +13,7 @@
   <body>
     <div class="container mt-5">
         <div class="row justify-content-center">
-            <div class="col-md-4 col-9">
+            <div class="col-md-4 col-12">
                 <div class="card card-login">
                     <div class="card-body">
                         <h5 class="card-title mb-4">Sign Up</h5>
@@ -51,7 +51,7 @@
                     </div>
                 </div>
                 <p class="text-center mt-4 mb-0">Copyright &copy; <?=date("Y") ?> All Right Reserved</p>
-                <p class="text-center mt-0 mb-0">Delta Crypto</p>
+                <p class="text-center mt-0 mb-0">Trading Crypto Bot</p>
             </div>
         </div>
     </div>
