fb1bf0a370fe1e3d1ae71338eb7fdad65ae9caf0
Deni Darmayana <74420243+denidarmayana@users.noreply.github.com>
2023-11-15 15:36:02 +0700

    depo afiliator

--- modules/afiliator/views/home.php
+++ modules/afiliator/views/home.php
@@ -41,22 +41,22 @@
                             </tr>
                             <tr>
                                 <td class=" text-primary">XBOT</td>
-                                <td class="text-center"><?=$this->app->getDeposit("XBOT") ?></td>
+                                <td class="text-center"><?=number_format($this->app->getDeposit("XBOT"),8) ?></td>
                                 <td class="text-center"><?=number_format($this->app->getMinus("XBOT"),8) ?></td>
                             </tr>
                             <tr>
                                 <td class=" text-primary">DOGE</td>
-                                <td class="text-center"><?=$this->app->getDeposit("DOGE") ?></td>
+                                <td class="text-center"><?=number_format($this->app->getDeposit("DOGE"),8) ?></td>
                                 <td class="text-center"><?=number_format($minus_doge,8) ?></td>
                             </tr>
                             <tr>
                                 <td class=" text-primary">TRX</td>
-                                <td class="text-center"><?=$this->app->getDeposit("TRX") ?></td>
+                                <td class="text-center"><?=number_format($this->app->getDeposit("TRX"),8) ?></td>
                                 <td class="text-center"><?=number_format($minus_trx,8) ?></td>
                             </tr>
                             <tr>
                                 <td class=" text-primary">BTT</td>
-                                <td class="text-center"><?=$this->app->getDeposit("BTT") ?></td>
+                                <td class="text-center"><?=number_format($this->app->getDeposit("BTT"),8) ?></td>
                                 <td class="text-center"><?=number_format($minus_btt,8) ?></td>
                             </tr>
                         </table>
